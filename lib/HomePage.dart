697fdbd6d10a81be38abc11b286df5fa5b3d739e
Kevin Li <kevinli4321@gmail.com>
2020-09-25 18:26:13 -0400

    Create a picker to choose region

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -5,8 +5,49 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: Text("Hello World!")),
       body: Center(
-        child: Text("Hello World!"),
+        child: Column(
+          children: [
+            RegionPicker(),
+          ],
+        ),
       ),
+    );
+  }
+}
+
+class RegionPicker extends StatefulWidget {
+  RegionPicker();
+
+  @override
+  _RegionPickerState createState() => _RegionPickerState();
+}
+
+class _RegionPickerState extends State<RegionPicker> {
+  String dropdownValue = 'Americas';
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButton<String>(
+      value: dropdownValue,
+      icon: Icon(Icons.language),
+      iconSize: 24,
+      elevation: 16,
+      style: TextStyle(color: Colors.deepPurple),
+      underline: Container(
+        height: 2,
+        color: Colors.deepPurpleAccent,
+      ),
+      onChanged: (String newValue) {
+        setState(() {
+          dropdownValue = newValue;
+        });
+      },
+      items: <String>['Americas', 'Europe', 'Oceania', 'Asia', 'Africa'].map<DropdownMenuItem<String>>((String value) {
+        return DropdownMenuItem<String>(
+          value: value,
+          child: Text(value),
+        );
+      }).toList(),
     );
   }
 }
