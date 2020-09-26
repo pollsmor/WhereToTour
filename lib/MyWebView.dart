77e960a2dd1136af08d0f8d51a2f03f6c38a175c
Kevin Li <kevinli4321@gmail.com>
2020-09-26 13:44:29 -0400

    bah I keep forgetting to add to git

---
+++ lib/MyWebView.dart
@@ -0,0 +1,28 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class MyWebView extends StatelessWidget {
+  final String title;
+  final String url;
+
+  final Completer<WebViewController> _controller = Completer<WebViewController>();
+
+  MyWebView({this.title, this.url});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: WebView(
+        initialUrl: url,
+        javascriptMode: JavascriptMode.unrestricted,
+        onWebViewCreated: (WebViewController webViewController) {
+          _controller.complete(webViewController);
+        },
+      ),
+    );
+  }
+}
